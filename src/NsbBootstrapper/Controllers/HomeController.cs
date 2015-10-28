393da3c1f9c267a3e86f0f0f5a5503a9db154044
Colin Higgins <colin.higgins@particular.net>
2015-10-28 17:22:38 -0400

    Initial commit of dumb UI

---
+++ src/NsbBootstrapper/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using NsbBootstrapper.Models;
+
+namespace NsbBootstrapper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private ConfigurationItem<T> MakeConfigurationItem<T>(T item) where T : struct, IConvertible
+        {
+            return new ConfigurationItem<T>()
+            {
+                Name = item.ToString(),
+                Value = item
+            };   
+        }
+
+        private List<ConfigurationItem<T>> MakeConfigurationList<T>(params T[] values) where T : struct, IConvertible
+        {
+            return values.Select(MakeConfigurationItem).ToList();
+        }
+
+        [HttpGet]
+        public JsonResult BootstrapBuilder()
+        {
+            var version5 = new VersionBuilderModel()
+            {
+                NServiceBusVersion = NServiceBusVersion.Five,
+            };
+
+            AddDefaultOptions(version5);
+
+            var version6 = new VersionBuilderModel()
+            {
+                NServiceBusVersion = NServiceBusVersion.Six,
+            };
+
+            AddDefaultOptions(version6);
+
+            var model = new BootstrapBuilderModel()
+            {
+                AvailableVersions = new List<VersionBuilderModel>()
+                {
+                    version5,
+                    version6
+                }
+            };
+
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        private void AddDefaultOptions(VersionBuilderModel version)
+        {
+            version.AvailablePersistence =
+                MakeConfigurationList(
+                    Persistence.NHibernate,
+                    Persistence.RavenDb,
+                    Persistence.AzureStorage);
+
+            version.AvailableSerializers =
+                MakeConfigurationList(
+                    Serializer.Json,
+                    Serializer.Xml,
+                    Serializer.Binary);
+
+            version.AvailableTransports =
+                MakeConfigurationList(
+                    Transport.Azure,
+                    Transport.Msmq,
+                    Transport.RabbitMq,
+                    Transport.Sql);
+        }
+    }
+}
---
+++ src/NsbBootstrapper/Models/BootstrapBuilderModel.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace NsbBootstrapper.Models
+{
+    public enum NServiceBusVersion
+    {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+    }
+    public enum Transport
+    {
+        Msmq,
+        Sql,
+        RabbitMq,
+        Azure,
+    }
+    public enum Persistence
+    {
+        InMemory,
+        NHibernate,
+        RavenDb,
+        AzureStorage,
+        Msmq
+    }
+    public enum Serializer
+    {
+        Xml,
+        Json,
+        Binary,
+    }
+
+    public class ConfigurationItem<T> where T : struct, IConvertible
+    {
+        public string Name { get; set; }
+        public T Value { get; set; }
+        public List<string> RelevantLinks { get; set; }
+    }
+
+    public class VersionBuilderModel
+    {
+        public NServiceBusVersion NServiceBusVersion { get; set; }
+        public List<ConfigurationItem<Transport>> AvailableTransports { get; set; }
+        public List<ConfigurationItem<Persistence>> AvailablePersistence { get; set; }
+        public List<ConfigurationItem<Serializer>> AvailableSerializers { get; set; }
+    }
+
+    public class BootstrapBuilderModel
+    {
+        public List<VersionBuilderModel> AvailableVersions { get; set; }
+    }
+
+    public class BootstrapModel
+    {
+        public NServiceBusVersion NServiceBusVersion { get; set; }
+        public Transport Transport { get; set; }
+        public Persistence Persistence { get; set; }
+        public Serializer Serializer { get; set; }
+    }
+
+    public class ConfigurationResultModel
+    {
+        public string NuGetInstallers { get; set; }
+        public string ConfigurationLogic { get; set; }
+    }
+}
