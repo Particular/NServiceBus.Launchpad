61f813784b94d891f349b86a58021929ffea2210
Colin Higgins <colin.higgins@particular.net>
2015-11-16 16:44:03 -0500

    Remove stubborn files

--- src/Website/app/nsbBootstrap.js
+++
@@ -1,9 +0,0 @@
-(function(ng) {
-    ng.module('nsbBootstrap', ['hljs'])
-        .config(function (hljsServiceProvider) {
-            hljsServiceProvider.setOptions({
-            // replace tab with 4 spaces
-            tabReplace: '    '
-        });
-    });
-})(angular);
